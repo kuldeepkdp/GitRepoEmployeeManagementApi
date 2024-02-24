989b83f63f6afa0c4d65a0b5353dded4adcb3e9c
kuldeep kumar <kuldeep.kdp@gmail.com>
2024-02-25 01:33:36 +0530

    updated print statement

--- employeeManagementBackend/src/main/java/com/kiyad/employeeManagementBackend/EmployeeManagementBackendApplication.java
+++ employeeManagementBackend/src/main/java/com/kiyad/employeeManagementBackend/EmployeeManagementBackendApplication.java
@@ -9,7 +9,7 @@ public class EmployeeManagementBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementBackendApplication.class, args);
 		
-		System.out.println("Hello World");
+		System.out.println("Hello kuldeep");
 	}
 
 }
